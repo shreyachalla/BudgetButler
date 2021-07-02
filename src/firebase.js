2efc8955f31a60cc5b006d2d019a99b62f01840b
Varsha V <63315944+vvattap@users.noreply.github.com>
2021-07-02 12:01:02 -0400

    added firestore configs to firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import "firebase/firestore";
 
 const app = firebase.initializeApp({
     apiKey: "AIzaSyDU3m77ICfjOgfMYXpQvK53zvTCAU-ILnc",
@@ -10,5 +11,10 @@ const app = firebase.initializeApp({
     appId: "1:727666401221:web:f8101f5a85a3892e3035ac"
 })
 
+// const firebase = Firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 export const auth = app.auth()
-export default app
+
+
+export {app, firebase, db };
+// export default app
