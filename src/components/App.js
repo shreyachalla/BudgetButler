9aafbb82816a52f60798799b55d676950d46d12c
Jodie Ritchie <jfritchie@wisc.edu>
2021-07-06 12:32:50 -0400

    commented out groceries path

--- src/components/App.js
+++ src/components/App.js
@@ -29,7 +29,7 @@ function App() {
           <Route path='/login' component={Login} />  
           <Route path='/overview' component={Overview} />
           <Route path='/reports' exact component={Reports} />
-          <Route path='/groceries' component={GrocerySetup} />
+          {/* <Route path='/groceries' component={GrocerySetup} /> */}
           <Route path='/settings' component={Settings} />
           </Switch>
           </AuthProvider>
