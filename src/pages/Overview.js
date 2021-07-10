1ec1e66a282dc26615b310c91e19ba1afef5f665
Varsha V <63315944+vvattap@users.noreply.github.com>
2021-07-10 16:44:48 -0400

    accessingNutrObjects

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -12,7 +12,7 @@ function Overview() {
   const fetchNutr=async()=>{
     const currentUser = firebase.auth().currentUser;  
   
-    const nutrients = [];
+   
 
     db.collection('users').doc(currentUser.uid).collection('groceries').get()
     .then((querySnapshot) => {
@@ -20,55 +20,61 @@ function Overview() {
       // console.log(doc.id, " => ", doc.data());
 
       let currentID = doc.id;
-      let nutrObj = {  ...doc.data(), ['id']: currentID}
+      let nutrObj = {   ...doc.data(),['id']: currentID}
       
       console.log("nutrObj NOT THE SET STATE HOOK: " + JSON.stringify(nutrObj))
       
- 
     setNutr(
-     
       nutr => [...nutr, nutrObj]
     );
-    console.log("nutr: " + JSON.stringify(nutr));
+    // console.log("nutr: " + JSON.stringify(nutr));
   }); 
 
 });
-    
-    
     // console.log("nutr: " + JSON.stringify(nutr))
     
 }
 
 const findMacros= ()=>{
-  nutr.id.names.name.map(function(mac) {
-    if (mac === "Fat" || mac === "Carbohydrates" || mac === "Protein" || mac === "Calories"){
-      // return true;
-      console.log(mac);
-    }
-    
+
+  // for each names object want to iterate though the array of macros
+   const nutrients = [];
+  // nutr.names.name.forEach((mac, index, nutr) =>{
+    nutr.names.forEach((mac, index, nutr)=>{
+      nutrients.push(nutr.id)
+      mac.names.forEach((eachMacros, index, mac)=>{
+        if (mac === "Fat" || mac === "Carbohydrates" || mac === "Protein" || mac === "Calories"){
+          nutrients.push(nutr.names[index])
+      }
+      })
+      
   })
-
+  
+console.log("NUTRIENTS: "+nutrients);
 }
-    
 
-
+findMacros();
+// console.log("nutr: " + JSON.stringify(nutr));
   return (
     // <div className='overview'>
     //   <h1>test</h1>
     // </div>
-
+    
     
     <div>
-      <h4>{findMacros}</h4>
-    
+      {/* <h4>{findMacros}</h4> */}
+      
       {
         nutr && nutr.map(nutr =>{
           return(
              <div className='overview'>
-                {/* <h4>{nutr.names[0].name}</h4>  */}
-                <h4>{nutr[0]}</h4>
-                {/* <h4>check</h4> */}
-
+                <h2>{nutr.id}</h2>
+                <h2>{nutr.names.name}</h2>
+                {/* <h2>{nutr.names}</h2> */}
+                {/* <h4>{nutr.names[0].name},</h4> 
+                <h4>{nutr.names[0].amount}</h4> 
+                <h4>{nutr.names[0].unit}</h4>  */}
+                
              </div>
             
           )
