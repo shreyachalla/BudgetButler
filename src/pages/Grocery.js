e1838aa7cb9c530e934d6204f8aa5920464e6db0
Jodie Ritchie <jfritchie@wisc.edu>
2021-07-19 20:39:02 -0400

    Added accordion to groceries

--- src/pages/Grocery.js
+++ src/pages/Grocery.js
@@ -4,26 +4,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function Grocery({ macroData, price }) {
   console.log("I'm in Grocery.js", JSON.stringify(macroData));
   return (
-    <>
-      <Card.Body>
-        <Card.Title variant={"dark"} className="text-center p-4">
-          Price $ {price}
-        </Card.Title>
-        {macroData.map((nutrition) => (
-          <ListGroup className="list-group-flush">
-            <ListGroupItem>
-              <h5>{nutrition.name}</h5>
-            </ListGroupItem>
-            <ListGroupItem>
-              Amount: {nutrition.amount} {nutrition.unit}
-            </ListGroupItem>
-            <ListGroupItem>
+    <Card.Body>
+      <Card.Title variant={"dark"} className="text-center p-4">
+        Price $ {price}
+      </Card.Title>
+      {macroData.map((nutrition) => (
+        <>
+          <h5>{nutrition.name}</h5>
+          <Card.Text>
+            Amount: {nutrition.amount} {nutrition.unit}
+          </Card.Text>
+
+          {/* <ListGroupItem>
               Percent of Daily Needs: {nutrition.percentOfDailyNeeds} %
-            </ListGroupItem>
-          </ListGroup>
-        ))}
-      </Card.Body>
-    </>
+            </ListGroupItem> */}
+        </>
+      ))}
+    </Card.Body>
   );
 }
 
